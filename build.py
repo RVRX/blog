8196fe84ef6d800ebb237cf58bde4f646aa70560
RVRX <colemanning@yandex.com>
2022-10-07 20:49:56 -0400

    feat: migrate away from ZacJW/markdown-html-action, have Actions run local py script instead

---
+++ build.py
@@ -0,0 +1,44 @@
+import os
+import markdown
+
+
+def piece_together(body):
+    prependee = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="/css/glow-style-terminal.css" rel="stylesheet">
+
+    <title>RVRX</title>
+</head>
+<body>
+<header><span style="color: #89982e">RVRX</span>@<span id="page-host">github.io</span> <span style="color: #89982e" id="page-path">~/blog</span> (<span style="color: #6d71be;">master</span>) $ <span style="color: #76b8cb"><a href="https://github.com/charmbracelet/glow#glow" target="_blank">glow</a> <span id="page-name">index.md</span></span></header>
+"""
+
+    appendee = """
+<script src="/js/glow-style-terminal-styler.js"></script>
+</body>
+</html>
+"""
+
+    return prependee + body + appendee
+
+
+def generate_html_from_file(path_to_file):
+    with open(path_to_file, "r", encoding="utf-8") as input_file:
+        text = input_file.read()
+    html = markdown.markdown(text)
+    with open(path_to_file.split(".")[0] + ".html", "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
+        output_file.write(piece_together(html))
+
+
+# BUILD INDEX FILE
+print('[build]: index.md')
+generate_html_from_file("index.md")
+# BUILD POST FILES
+with os.scandir('posts/') as entries:
+    for entry in entries:
+        print('[build]: ' + entry.name)
+        generate_html_from_file("posts/" + entry.name)
---
+++ js/glow-style-terminal-styler.js
@@ -0,0 +1,28 @@
+// add "lonesome-link" class to untitled links
+let links = document.body.getElementsByTagName("a");
+for (let link of links) {
+    if (link.innerText === "") {
+        link.classList.add("lonesome-link");
+    }
+}
+
+// auto-add anchor tag functionality
+let headings = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
+for (let i = 0; i < headings.length; i++) {
+    headings[i].id = headings[i].innerText;
+    headings[i].addEventListener('click', function () {
+        document.location.hash = headings[i].id;
+    });
+}
+
+// dynamic shell setup [abs top left line dude]
+// ex if page is: http://localhost:63342/blog/posts/unraid.html
+const pathname = window.location.pathname.split('/') // ex: [ "", "blog", "posts", "unraid.html" ]
+const name = pathname.pop().split('.')[0] + '.md' // "unraid.md"
+const path = pathname.join('/') // "/blog/posts"
+document.getElementById('page-path').innerText = '~' + path
+document.getElementById('page-name').innerText = name
+document.getElementById('page-host').innerText = window.location.host
+
+// set page title to that of first 'h1'
+document.title = document.getElementsByTagName('h1')[0].innerText
