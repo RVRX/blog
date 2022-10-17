ca3480de1f7fc8d96ec87e77d7d5846784113f14
RVRX <colemanning@yandex.com>
2022-10-17 15:43:01 -0400

    chore: refactor build.py html builder to be a little more pretty

--- build.py
+++ build.py
@@ -6,41 +6,38 @@ from os.path import getctime
 
 
 def piece_together(body, post_list, page_title="RVRX", page_description="", page_image="https://blog.rvrx.dev/img/about/terminal-example.png"):
-    prependee = """<!DOCTYPE html>
+
+    file_body = f"""<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Dev Blog of Cole Manning (RVRX)">
-    <meta name="keywords" content="Cole Manning, Blog, RVRX, devblog, dev-blog, WPI, Worcester Polytechnic Institute">"""
-
-    og_title = f'<meta property="og:title" content="{page_title}">'
-    og_sitename = f'<meta property="og:site_name" content="RVRX.">'
-    og_url = f'<meta property="og:url" content=https://blog.rvrx.dev>'
-    og_desc = f'<meta property="og:description" content="{page_description}">'
-    og_type = '<meta property="og:type" content="article">'
-    og_image = f'<meta property="og:image" content={page_image}>'
-    open_graph = f'\n\t{og_title}\n\t{og_sitename}\n\t{og_url}\n\t{og_desc}\n\t{og_type}\n\t{og_image}'
-
-    post_prependee = """
+    <meta name="keywords" content="Cole Manning, Blog, RVRX, devblog, dev-blog, WPI, Worcester Polytechnic Institute">
+    
+    <meta property="og:title" content="{page_title}">
+    <meta property="og:site_name" content="RVRX.">
+    <meta property="og:url" content=https://blog.rvrx.dev>
+    <meta property="og:description" content="{page_description}">
+    <meta property="og:type" content="article">
+    <meta property="og:image" content={page_image}>
+    
     <link href="/css/glow-style-terminal.css" rel="stylesheet">
-    <title>RVRX</title>
+    <title>{page_title}</title>
 </head>
+
 <body class="body">
 <main class="main">
 <header><a href="/"><span style="color: #89982e">RVRX</span>@<span class="page-host">localhost</span> <span style="color: #89982e" id="page-path">~/blog</span> (<span style="color: #6d71be;">master</span>)</a> $ <span style="color: #76b8cb"><a href="https://github.com/charmbracelet/glow#glow" target="_blank">glow</a> <span id="page-name">index.md</span></span></header>
 <div class="generated-content">
-"""
-
-    appendee = """
+{body}
 </div>
 </main>
 <div class="sidebar">
     <div id="sidebar-shell">
         <span style="color: #89982e">~/posts</span> $ <span style="color: #76b8cb">ls -l</span>
-        <div id="post-listing-ol">"""
-
-    appendee_the_second = """
+        <div id="post-listing-ol">
+        {post_list}
         </div>
         <aside id="heading-listing">
             <span style="color: #89982e">~</span> $ <span style="color: #76b8cb">cd ../contents</span><br>
@@ -63,7 +60,7 @@ def piece_together(body, post_list, page_title="RVRX", page_description="", page
 </html>
 """
 
-    return prependee + open_graph + post_prependee + body + appendee + post_list + appendee_the_second
+    return file_body
 
 
 def generate_html_from_file(path_to_file, post_list):
